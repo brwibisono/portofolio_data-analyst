b185486e1595aca3dd7f8fbe05ffe515cf139baf
brwibisono <br.wibisono@gmail.com>
2026-01-20 00:39:43 +0700

    Create 00_base.sql

---
+++ brwibisono_analytics/03_sales_funnel/01_sql/00_base.sql
@@ -0,0 +1,13 @@
+WITH base_funnel AS (
+  SELECT
+    u.id AS user_id,
+    o.order_id,
+    oi.order_id AS paid_order_id
+  FROM brwibisono.raw_thelook.users u
+  LEFT JOIN brwibisono.raw_thelook.orders o
+    ON u.id = o.user_id
+  LEFT JOIN brwibisono.raw_thelook.order_items oi
+    USING(order_id)
+)
+
+SELECT * FROM base_funnel;
